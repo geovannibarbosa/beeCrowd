bea50426eca558f3f0ccaa13e62b241e516c999f
Geovanni Barbosa Reis <143896861+geovannibarbosa@users.noreply.github.com>
2024-01-26 10:08:13 -0300

    Delete 1010.py

--- 1010.py
+++
@@ -1,6 +0,0 @@
-#beecrowd exercise: 1010 - Simple Calculate
-
-valueA, valueB, valueC = map(float, input().split());
-valueD, valueE, valueF = map(float, input().split());
-toPay = (valueB * valueC) + (valueE * valueF);
-print(f'VALOR A PAGAR: R$ {toPay:.2f}');
