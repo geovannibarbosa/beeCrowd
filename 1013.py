71cb34a19597f69b3e6b36622d35f1ab5e8f9867
Geovanni Barbosa Reis <143896861+geovannibarbosa@users.noreply.github.com>
2024-03-11 15:50:33 -0300

    Update 1013.py

--- 1013.py
+++ 1013.py
@@ -1,6 +1,8 @@
 #beecrowd exercise: 1013 - The Greatest
 
 valueA, valueB, valueC = map(int, input().split());
-print(valueA);
-print(valueB);
-print(valueC);
+
+maiorAB = int((valueA + valueB + abs(valueA - valueB)) / 2);
+maiorABC = int((maiorAB + valueC + abs(maiorAB - valueC)) /2);
+
+print(maiorABC, "eh o maior");
